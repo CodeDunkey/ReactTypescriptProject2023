f14ee0050526aa31ceb09cc423aff1066d5f8846
Tastan Kodersen <andmarknu@hotmail.com>
2024-01-09 22:05:08 +0100

    09.01.2023 22:05

--- mm-vision/src/components/Main/MainPictureSlide/MainPicture.tsx
+++ mm-vision/src/components/Main/MainPictureSlide/MainPicture.tsx
@@ -1,64 +1,68 @@
 import './MainPictureSlide.scss'
+import { ShowSliderPicturesArray } from './MainSliderPictures';
+
 export default function MainPicture() {
-    let slideIndex: number = 1;
-    
-    
-    // function plusSlides(n: number) {
-    //     showSlides(slideIndex += n);
+    // let slideIndex: number = 1;
+
+
+    // // function plusSlides(n: number) {
+    // //     showSlides(slideIndex += n);
+    // // }
+    // function currentSlide(n: number) {
+    //     showSlides(slideIndex = n);
     // }
     
-    function currentSlide(n: number) {
-        showSlides(slideIndex = n);
-    }
+    // function showSlides(n: number) {
+    //     // let i;
+        
+    //     // console.log(n)
+    //     // let slides = document.getElementsByClassName("mySlides");
+
+    //     // let dots = document.getElementsByClassName("sliderDot");
+    //     // // if (n > slides.length) { slideIndex = 1 }
+        
+    //     // if (n < 1) { slideIndex = slides.length }
+
+    //     // const findPict = slides
+        
+    //     // for (i = 0; i < slides.length; i++) {
+    //     //     // console.log(slides)
+    //     //     slides[i].className = "block";
+    //     // }
+
+    //     // for (i = 0; i < dots.length; i++) {
+    //         //     dots[i].className = dots[i].className.replace(" active", "");
+    //         // }
+            
+    //         // slides[slideIndex - 1].className = "block";
+            
+    //         // dots[slideIndex - 1].className += " active";
+    // }
     
-    function showSlides(n: number) {
-        let i;
-        
-        console.log(n)
-        let slides = document.getElementsByClassName("mySlides");
-
-        let dots = document.getElementsByClassName("sliderDot");
-        // if (n > slides.length) { slideIndex = 1 }
-        
-        // if (n < 1) { slideIndex = slides.length }
-        
-        const findSlider = 
-
-        // for (i = 0; i < slides.length; i++) {
-        //     // console.log(slides)
-        //     slides[i].className = "block";
-        // }
-        
-        // for (i = 0; i < dots.length; i++) {
-        //     dots[i].className = dots[i].className.replace(" active", "");
-        // }
-        
-        // slides[slideIndex - 1].className = "block";
-        
-        // dots[slideIndex - 1].className += " active";
-    }
-
-    showSlides(slideIndex);
-
-
-
+    // showSlides(slideIndex);
+    const slidePictures = ShowSliderPicturesArray.map(pict => { 
+        return(
+            <div className='mySlides'>
+                <img className='image' key={pict.id} style={{backgroundImage: `url(${pict.src})`, backgroundSize: pict.pictureSize }}></img>
+            </div>
+        )})
+    
+    
+    // console.log("slidePictures", slidePictures)
+    
     return (
         <div className="mainPictureSlideContainer">
-            <div className='mySlides'>
-                <img className='imageOne'></img>
-            </div>
-            <div className='mySlides'>
-                <img className='imageTwo'></img>
-            </div>
-            <div className='mySlides'>
-                <img className='imageThree'></img>
-            </div>
+            {slidePictures}
             <div className='sliderDotWrapper'>
-                <div className='sliderDot' onClick={()=>currentSlide(1)}></div>
-                <div className='sliderDot' onClick={()=>currentSlide(2)}></div>
-                <div className='sliderDot' onClick={()=>currentSlide(3)}></div>
+                <div className='sliderDot' ></div>
+                <div className='sliderDot' ></div>
+                <div className='sliderDot' ></div>
             </div>
 
         </div>
     )
 }
+
+// onClick={() => currentSlide(1)}
+// onClick={() => currentSlide(2)}
+// onClick={() => currentSlide(3)}
--- mm-vision/src/components/Main/MainPictureSlide/MainSliderPictures.ts
+++ mm-vision/src/components/Main/MainPictureSlide/MainSliderPictures.ts
@@ -1,6 +1,8 @@
-
+import picture1 from '../../../Assets/nySlidePict1.png'
+import picture2 from '../../../Assets/nySlidePict2.png'
+import picture3 from '../../../Assets/nySlidePict3.png'
 export const ShowSliderPicturesArray = [
-    {picture: '"../../../Assets/nySlidePict1.png"'},
-    {picture: "../../../Assets/nySlidePict2.png"},
-    {picture: "../../../Assets/nySlidePict3.png"}
+    {id: 1, src: picture1, pictureSize: "cover"},
+    {id: 2, src: picture2, pictureSize: "cover"},
+    {id: 3, src: picture3, pictureSize: "cover"}
 ]
