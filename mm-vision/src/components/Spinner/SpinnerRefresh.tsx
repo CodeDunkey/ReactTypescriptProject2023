b10bc937ffbc406c5bbf91475303c905d8b97e9c
Andreas <andmarknu@hotmail.com>
2024-01-26 15:11:27 +0100

    26.01.2024 15:10

--- mm-vision/src/components/Spinner/SpinnerRefresh.tsx
+++ mm-vision/src/components/Spinner/SpinnerRefresh.tsx
@@ -2,36 +2,36 @@ import { useEffect, useState } from "react";
 import { SpinnerLoadingIcon } from "./Spinner";
 
 //#region // Spinner before page refresh.
-interface SpinnerProps {}
+interface SpinnerProps { }
 
 export const SpinnerSiteRefresh: React.FC<SpinnerProps> = () => {
-  
+
   const [isSpinnerVisible, setSpinnerVisible] = useState(false)
-  
+
   const showSpinnerFor3Seconds = () => {
-      setSpinnerVisible(true)
-      
-      setTimeout(() => {
-        setSpinnerVisible(false)
-      }, 3000)
+    setSpinnerVisible(true)
+
+    setTimeout(() => {
+      setSpinnerVisible(false)
+    }, 3000)
   };
-  
+
   useEffect(() => {
     const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-          showSpinnerFor3Seconds();
-          event.returnValue = '????'
-        }
-        
-        window.addEventListener('beforeunload', handleBeforeUnload);
-        return () => {
-          window.removeEventListener('beforeunload', handleBeforeUnload)
-      }
-    })
+      showSpinnerFor3Seconds();
+      event.returnValue = ''
+    }
 
-    return(
-      <>
-          {isSpinnerVisible && SpinnerLoadingIcon()}
-      </>
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [])
+
+  return (
+    <>
+      {isSpinnerVisible && SpinnerLoadingIcon()}
+    </>
   )
 }
 //#endregion
